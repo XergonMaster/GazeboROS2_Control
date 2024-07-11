70335cf4a19c8014ff6e4dfa0b37805542f2cb4e
David S.D <david.salazar51@eia.edu.co>
2024-07-11 14:37:22 -0500

    chore: Add Gazebo mobile_robot_examples package files New name package

--- Gazebo/mobile_robot_examples/setup.py
+++ Gazebo/mobile_robot_examples/setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
-package_name = 'ros2controllers_tutorial'
+
+package_name = 'mobile_robot_examples'
 
 setup(
     name=package_name,
@@ -11,8 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro.urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
